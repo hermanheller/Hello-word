d080a3e328056c082c8922f95bc77fb3a1ecf28e
hermanheller <kalle.hynninen@gmail.com>
2026-04-18 12:07:55 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello World!")
-=======
-print("Hello world!")
->>>>>>> intermediate
-=======
-print("Hello world!")
->>>>>>> origin
-=======
 print("Hello world!")
 print("Hello once again")
->>>>>>> nm/feature/add-logging
+
